0acf9834a8c093e33124a1adf0b64eb9cdba070d
Gustavo Vera <ing.vera.velasco@gmail.com>
2018-06-12 23:03:24 -0500

    Explain logging problem and basic decorator

---
+++ sample.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    """
+    Adds x and y
+    :param x: a number
+    :param y: a number
+    :return:
+    """
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def mul(x, y):
+    return x * y
